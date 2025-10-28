88bcf89ac58685138dda5aab38d28a9d0cbff5b3
Chanalizain <inchanaliza@gmail.com>
2025-10-29 00:24:00 +0700

    pure dart set up

---
+++ hospital_appointment/bin/hospital_appointment.dart
@@ -0,0 +1,5 @@
+import 'package:hospital_appointment/hospital_appointment.dart' as hospital_appointment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hospital_appointment.calculate()}!');
+}
---
+++ hospital_appointment/test/hospital_appointment_test.dart
@@ -0,0 +1,8 @@
+import 'package:hospital_appointment/hospital_appointment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
